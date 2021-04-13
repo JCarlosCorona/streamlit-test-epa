e1d630f9b4b39ab1d48b955e3a2c0d5a9db245e6
JCarlosCorona <josecarloscorona@gmail.com>
2021-04-13 18:25:36 -0500

    short code

--- first_app.py
+++ first_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
-import time
 
 """
 # My first app
@@ -18,8 +17,8 @@ df = pd.DataFrame({
 df
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
 
@@ -31,35 +30,7 @@ st.map(map_data)
 
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
 
     chart_data
-
-option = st.sidebar.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected:', option
-
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Press me?')
-if pressed:
-    right_column.write("Woohoo!")
-
-expander = st.beta_expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
-
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
